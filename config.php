f558df99e0c09b76e25eaa17d8ba1257b2b87f6c
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2025-01-11 23:22:06 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'api_url' => 'http://127.0.0.1:5000', // Replace with your actual API URL
+    'api_url' => 'http://127.0.0.1:5000/api', // Replace with your actual API URL
     'api_key' => 'YOUR_API_KEY_HERE', // Replace 'YOUR_API_KEY_HERE' with your actual API key
     'recaptcha_site_key' => 'YOUR_SITE_KEY', // Replace 'YOUR_SITE_KEY' with your actual Site Key for reCAPTCHA
     'recaptcha_secret_key' => 'YOUR_SECRET_KEY', // Replace 'YOUR_SECRET_KEY' with your actual Secret Key for reCAPTCHA
