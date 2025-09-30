4c217f7d0c64dd21c14d2d79b03dd61466fa07d4
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2025-09-30 19:05:01 +0500

    Refactor config.php to use getenv for settings
    Updated configuration to use environment variables for sensitive data.

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'api_url' => 'http://127.0.0.1:5000/api', // Replace with your actual API URL
-    'api_key' => 'YOUR_API_KEY_HERE', // Replace 'YOUR_API_KEY_HERE' with your actual API key
-    'recaptcha_site_key' => 'YOUR_SITE_KEY', // Replace 'YOUR_SITE_KEY' with your actual Site Key for reCAPTCHA v3
-    'recaptcha_secret_key' => 'YOUR_SECRET_KEY', // Replace 'YOUR_SECRET_KEY' with your actual Secret Key for reCAPTCHA v3
-    'captcha_type' => 'recaptcha' // Change to 'recaptcha' to use reCAPTCHA v3
+    'api_url' => getenv('API_URL'),
+    'api_key' => getenv('API_KEY'),
+    'recaptcha_site_key' => getenv('RECAPTCHA_SITE_KEY'),
+    'recaptcha_secret_key' => getenv('RECAPTCHA_SECRET_KEY'),
+    'captcha_type' => getenv('CAPTCHA_TYPE')
 ];
 ?>
